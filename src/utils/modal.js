93ef8999041c114463cee162add5d58066fc3a41
bo.wang002 <bo.wang002@daddylab.com>
2024-11-04 14:34:10 +0800

    feat: 组件modal

--- src/utils/modal.js
+++ src/utils/modal.js
@@ -1,14 +1,21 @@
 import { h } from 'vue'
 
-/** 二次确认等 */
-export const waitConfirmModal = ({ title = '警告', content = '你确定？', type = 'warning', ...props } = {}) => {
+/**
+ *
+ * @param title 标题
+ * @param content 提示内容
+ * @param type 提示类型 success | warning | error
+ * @param config 其他配置
+ * @returns {Promise<unknown>}
+ */
+export const waitConfirmModal = ({ title = '警告', content = '你确定？', type = 'warning', ...config } = {}) => {
   return new Promise((resolve, reject) => {
     const ins = $dialog[type]({
       title,
       content,
       positiveText: '确定',
       negativeText: '取消',
-      ...props,
+      ...config,
       onPositiveClick: () => {
         ins.loading = true
         resolve()
@@ -22,10 +29,13 @@ export const waitConfirmModal = ({ title = '警告', content = '你确定？', t
 }
 
 /**
- * 渲染组件弹窗
- * @param {*} render 需要渲染的component固定 emit['confirm', 'close']
- * @param {*} param
- * @returns
+ * @param render 需要渲染的component固定 emit['confirm', 'close']
+ * @param props 渲染组件的props
+ * @param title $modal标题
+ * @param preset $modal类型 dialog | card
+ * @param style $modal样式自定义
+ * @param config modal其他配置
+ * @returns {Promise<unknown>}
  */
 export const invokeModal = (
   { render, ...props },
