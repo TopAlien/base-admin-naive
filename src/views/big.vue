da671b18f0408c192846f18fb52d2da3db4e6511
bo.wang002 <bo.wang002@daddylab.com>
2025-07-15 16:35:25 +0800

    feat: 123

--- src/views/big.vue
+++ src/views/big.vue
@@ -15,38 +15,38 @@
   >
     <n-descriptions-item label="0.1 + 0.2">
       {{ 0.1 + 0.2 }}
-      <p>{{ new BigNumber(0.1).plus(0.2) }}</p>
+      <p>{{ BigNumber(0.1).plus(0.2) }}</p>
     </n-descriptions-item>
 
     <n-descriptions-item label="1.1 - 0.2">
       {{ 1.1 - 0.2 }}
-      <p>{{ new BigNumber(1.1).minus(0.2) }}</p>
+      <p>{{ BigNumber(1.1).minus(0.2) }}</p>
     </n-descriptions-item>
 
     <n-descriptions-item label="1.1 * 100">
       {{ 1.1 * 100 }}
-      <p>{{ new BigNumber(1.1).times(100) }}</p>
+      <p>{{ BigNumber(1.1).times(100) }}</p>
     </n-descriptions-item>
 
     <n-descriptions-item label="12.12 / 100">
       {{ 12.12 / 100 }}
-      <p>{{ new BigNumber(12.12).div(100) }}</p>
+      <p>{{ BigNumber(12.12).div(100) }}</p>
     </n-descriptions-item>
 
     <n-descriptions-item label="1.1 / 100">
       {{ 1.1 / 100 }}
-      <p>{{ new BigNumber(1.1).div(100) }}</p>
+      <p>{{ BigNumber(1.1).div(100) }}</p>
     </n-descriptions-item>
 
     <n-descriptions-item label="0.9 / 100">
       {{ 0.9 / 100 }}
-      <p>{{ new BigNumber(0.9).div(100) }}</p>
+      <p>{{ BigNumber(0.9).div(100) }}</p>
     </n-descriptions-item>
 
     <n-descriptions-item label="1.1 * 100 - 0.9 * 100 ">
       {{ 1.1 * 100 - 0.9 * 100 }}
       <p>
-        {{ new BigNumber(1.1).times(100).minus(new BigNumber(0.9).times(100)) }}
+        {{ BigNumber(1.1).times(100).minus(BigNumber(0.9).times(100)) }}
       </p>
     </n-descriptions-item>
 
@@ -55,7 +55,7 @@
       <n-input v-model:value="plus.b" />
       {{ Number(plus.a) + Number(plus.b) }}
       <p>
-        {{ new BigNumber(plus.a).plus(plus.b) }}
+        {{ BigNumber(plus.a).plus(plus.b) }}
       </p>
     </n-descriptions-item>
 
@@ -64,7 +64,7 @@
       <n-input v-model:value="minus.b" />
       {{ Number(minus.a) - Number(minus.b) }}
       <p>
-        {{ new BigNumber(minus.a).minus(minus.b) }}
+        {{ BigNumber(minus.a).minus(minus.b) }}
       </p>
     </n-descriptions-item>
 
@@ -73,7 +73,7 @@
       <n-input v-model:value="times.b" />
       {{ Number(times.a) * Number(times.b) }}
       <p>
-        {{ new BigNumber(times.a).times(times.b) }}
+        {{ BigNumber(times.a).times(times.b) }}
       </p>
     </n-descriptions-item>
 
@@ -82,7 +82,7 @@
       <n-input v-model:value="div.b" />
       {{ Number(div.a) / Number(div.b) }}
       <p>
-        {{ new BigNumber(div.a).div(div.b) }}
+        {{ BigNumber(div.a).div(div.b) }}
       </p>
     </n-descriptions-item>
 
